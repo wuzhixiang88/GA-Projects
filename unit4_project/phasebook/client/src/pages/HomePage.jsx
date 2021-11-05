1f14c0cd97305673103c0ae192fd89044edcac4c
wuzhixiang88 <wuzhixiang88@gmail.com>
2021-11-05 14:45:31 +0800

    setup sign up modal for create new account button

--- unit4_project/phasebook/client/src/pages/HomePage.jsx
+++ unit4_project/phasebook/client/src/pages/HomePage.jsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
 
 const HomePage = () => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
     <Container fluid className="bg-light">
       <Row className="d-flex justify-content-center py-5">
@@ -20,7 +26,7 @@ const HomePage = () => {
         </Col>
         <Col md={2} className="bg-white rounded">
           <Form className="mt-3">
-            <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Group className="mb-3">
               <Form.Control
                 type="email"
                 placeholder="Enter email"
@@ -28,7 +34,7 @@ const HomePage = () => {
               />
             </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Group className="mb-3">
               <Form.Control type="password" placeholder="Password" />
             </Form.Group>
           </Form>
@@ -40,10 +46,67 @@ const HomePage = () => {
           </div>
 
           <div className="d-grid gap-2 my-3">
-            <Button variant="success" type="submit" className="fw-bold">
+            <Button
+              variant="success"
+              type="submit"
+              onClick={handleShow}
+              className="fw-bold"
+            >
               Create New Account
             </Button>
           </div>
+
+          <Modal
+            show={show}
+            onHide={handleClose}
+            backdrop="static"
+            keyboard={false}
+          >
+            <Modal.Header closeButton>
+              <Modal.Title>Sign Up</Modal.Title>
+            </Modal.Header>
+            <Modal.Body className="show-grid">
+              <Container>
+                <Row>
+                  <Col xs={12} md={6} className="ps-0">
+                    <Form.Group className="mb-3">
+                      <Form.Control
+                        type="text"
+                        placeholder="First Name"
+                        autoFocus={true}
+                      />
+                    </Form.Group>
+                  </Col>
+                  <Col xs={12} md={6} className="px-0">
+                    <Form.Group className="mb-3">
+                      <Form.Control type="text" placeholder="Surname" />
+                    </Form.Group>
+                  </Col>
+                </Row>
+
+                <Row>
+                  <Col className="px-0">
+                    <Form.Group className="mb-3">
+                      <Form.Control type="text" placeholder="Email Address" />
+                    </Form.Group>
+                  </Col>
+                </Row>
+
+                <Row>
+                  <Col className="px-0">
+                    <Form.Group className="mb-3">
+                      <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+                  </Col>
+                </Row>
+              </Container>
+            </Modal.Body>
+            <Modal.Footer className="d-flex justify-content-center">
+              <Button variant="success" className="px-5">
+                Sign Up
+              </Button>
+            </Modal.Footer>
+          </Modal>
         </Col>
       </Row>
     </Container>
